cc669f0c230d7a7cfd132380ec5b50d232521ae9
SGM <sgmthesecond@gmail.com>
2026-04-23 17:12:35 +0530

    added base window and quit button

---
+++ main.py
@@ -0,0 +1,29 @@
+#  2nd GAME TRY!! stopped flappy bird bec main goal is make as many games as possibles
+#  so to learn new principles and also use previously learned lesson  I AM STARTING GAME 2!!
+# time - finish in 3 days fast before teen camp
+#and also coding gives more joy than gaming! so now i will leave gaming and full time think about coding!
+#AND ALSO USING GIT FOR FIRST TIME IN REAL GAME
+
+
+import pygame
+from sys import exit
+
+gameactive = True
+screen = pygame.display.set_mode((600,600))
+
+#playerrect = pygame.rect()
+
+while gameactive:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit
+            exit()
+        if gameactive == True:
+            pass
+
+
+
+
+
+
+
