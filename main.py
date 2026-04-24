6a722bc50ab9665312ff164df2496f00397d16f1
SGM <sgmthesecond@gmail.com>
2026-04-24 16:37:40 +0530

    made ball rect for hitbox of ball and to detect collision

--- main.py
+++ main.py
@@ -20,11 +20,23 @@ enemyrect = pygame.Rect(50,200, 50, 150)
 ballx = 300
 bally = 300
 ballrad = 15
+balldirec = 2
+
 
 #BUG HERE
 def ball_ai():
-    global ballx
-    ballx += 1
+    global ballx, balldirec, ballrect
+
+    ballx += balldirec
+
+    #if playerrect.collidepoint((ballx, bally)):
+    #    balldirec = -(balldirec)
+
+    if playerrect.colliderect(ballrect):
+        balldirec = -(balldirec)
+        print('collision')
+    
+
 
 def enemyai():
     pass
@@ -32,7 +44,11 @@ def enemyai():
 #MAIN GAME WHILE LOOP
 while gameactive:
 
-    #TEST:putting all variable here bec of local variable glitch
+    #varibles that need to be constnatly updated are put inside loop and they are first
+    ballrectx = ballx - ballrad
+    ballrecty = bally - ballrad
+
+    ballrect = pygame.Rect(ballrectx, ballrecty, (ballrad * 2), (ballrad * 2))
     
 
     #MAIN EVENT LOOP
@@ -59,7 +75,10 @@ while gameactive:
 
         pygame.draw.rect(screen, 'orange', playerrect)
         pygame.draw.rect(screen, 'orange', enemyrect)
+
+        pygame.draw.rect(screen, 'white', ballrect) #hitbox rect of ball
         pygame.draw.circle(screen, 'yellow', (ballx, bally), ballrad)
+        
 
     pygame.display.update()
     clock.tick(60)
