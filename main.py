3241e09589cbf4ddd0213dce15470a8e407de487
SGM <sgmthesecond@gmail.com>
2026-04-24 15:38:36 +0530

    added basic ball movement using func

--- main.py
+++ main.py
@@ -8,19 +8,32 @@
 import pygame
 from sys import exit
 
+#ALL VARIABLES AND DECLARATION/CREATION OF STUFF
+
 gameactive = True
 screen = pygame.display.set_mode((600,600))
 clock = pygame.time.Clock()
 
 playerrect = pygame.Rect(500,200, 50, 150)
 enemyrect = pygame.Rect(50,200, 50, 150)
-ballpos = (300,300)
+
+ballx = 300
+bally = 300
 ballrad = 15
 
+#BUG HERE
 def ball_ai():
+    global ballx
+    ballx += 1
+
+def enemyai():
     pass
 
+#MAIN GAME WHILE LOOP
 while gameactive:
+
+    #TEST:putting all variable here bec of local variable glitch
+    
 
     #MAIN EVENT LOOP
     for event in pygame.event.get():
@@ -29,20 +42,24 @@ while gameactive:
             exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_UP:
-                playerrect.y -= 10
+                playerrect.y -= 20
             elif event.key == pygame.K_DOWN:
-                playerrect.y += 10
+                playerrect.y += 20
 
-        # black background and clears screen bec no bg now
-        screen.fill((0, 0, 0))  
+    # black background and clears screen bec no bg now
+    screen.fill((0, 0, 0))  
+
+    #animations
+    ball_ai()
+    
 
 
-        if gameactive == True:
-            #draw all entities
+    if gameactive == True:
+        #draw all entities
 
-            pygame.draw.rect(screen, 'orange', playerrect)
-            pygame.draw.rect(screen, 'orange', enemyrect)
-            pygame.draw.circle(screen, 'yellow', ballpos, ballrad)
+        pygame.draw.rect(screen, 'orange', playerrect)
+        pygame.draw.rect(screen, 'orange', enemyrect)
+        pygame.draw.circle(screen, 'yellow', (ballx, bally), ballrad)
 
     pygame.display.update()
     clock.tick(60)
